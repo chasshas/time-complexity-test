2b22d15e0a047dcd146b2b887770f23eb34e4a05
chas <chssshas@naver.com>
2025-06-06 16:00:55 +0900

    time complexity test functions added

--- timetest.py
+++ timetest.py
@@ -1,4 +1,6 @@
 import datetime
+import math
+
 def meta_print(data: float):
     print(format(data, ".100f"))
 
@@ -10,3 +12,28 @@ def time_test(code: str, n: int)->float:
     end = datetime.datetime.now()
     result = end-start
     return result.total_seconds()
+
+def constant(n: int, code: str):
+    exec(code)
+
+def logarithmic(n,base: int, code: str):
+    i = 1
+    while i < n:
+        i *= base
+        exec(code)
+
+def linear(n, code: str):
+    for i in range(n):
+        exec(code)
+
+def factorial(n: int, code: str):
+    for i in range(math.factorial(n)):
+        exec(code)
+
+def square(n: int, exp: int, code: str):
+    for i in range(n**exp):
+        exec(code)
+
+def exponential(n: int, base: int, code: str):
+    for i in range(base**n):
+        exec(code)
